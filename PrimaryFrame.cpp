17e4b219cf163dafdae86241fa8775231126ada1
Jonathan Tucker <jtuck2k@gmail.com>
2021-12-22 12:33:54 -0500

    Frame is now 720p and centered on the screen

--- PrimaryFrame.cpp
+++ PrimaryFrame.cpp
@@ -1,8 +1,9 @@
 #include "PrimaryFrame.h"
 
-PrimaryFrame::PrimaryFrame() : wxFrame(nullptr, wxID_ANY, "RxHelper")
+PrimaryFrame::PrimaryFrame() : wxFrame(nullptr, wxID_ANY, "RxHelper", wxPoint(0,0) , wxSize(1280, 720))
 {
-
+	// Center the window on the screen.
+	this->Center();
 }
 
 PrimaryFrame::~PrimaryFrame()

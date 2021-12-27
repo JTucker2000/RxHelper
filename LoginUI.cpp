5664e8663917d873909a51c58d4b0dd1ab1a9e29
Jonathan Tucker <jtuck2k@gmail.com>
2021-12-27 15:03:31 -0500

    Small changes to LoginUI + fixed mem leak

--- LoginUI.cpp
+++ LoginUI.cpp
@@ -7,12 +7,15 @@ LoginUI::LoginUI(wxWindow* parent)
 		exit(-1);
 	}
 
-	vertical_sizer = new wxBoxSizer(wxVERTICAL);
+	// Panel for login screen.
+	wxPanel* login_panel = new wxPanel(parent, wxID_ANY, wxDefaultPosition, wxSize(300,150), 2621440L, "test");
 
+	// Sizer for login screen.
+	vertical_sizer = new wxBoxSizer(wxVERTICAL);
 	vertical_sizer->AddStretchSpacer(1);
 	vertical_sizer->Add
 	(
-		new wxPanel(parent, wxID_ANY, wxDefaultPosition, wxSize(100, 50), 2621440L, "test"),
+		login_panel,
 		wxSizerFlags().Center().Border(wxALL, 10)
 	);
 	vertical_sizer->AddStretchSpacer(1);
@@ -25,5 +28,5 @@ wxBoxSizer* LoginUI::getSizer()
 
 LoginUI::~LoginUI() 
 {
-
+	
 }
--- PrimaryFrame.cpp
+++ PrimaryFrame.cpp
@@ -5,11 +5,12 @@ PrimaryFrame::PrimaryFrame() : wxFrame(nullptr, wxID_ANY, "RxHelper", wxPoint(0,
 	login_ui = new LoginUI(this);
 
 	SetSizer(login_ui->getSizer());
+	SetMinSize(login_ui->getSizer()->GetMinSize());
 
 	Center();
 }
 
 PrimaryFrame::~PrimaryFrame()
 {
-
+	delete(login_ui);
 }
